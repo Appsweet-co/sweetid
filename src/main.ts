29833b9f7b5d430a9050fe79d6f3e42115e36581
dperuo <derek@appsweet.co>
2023-12-22 23:14:42 -0600

    refactor: (main.ts) Use ternary operator

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,5 @@ import { generate, getIdLength, isValid } from "./service.ts";
 export function sweetid(size: SweetIdSize = "short"): SweetId {
   const id = generate(getIdLength(size));
 
-  if (isValid(id)) return id;
-
-  return sweetid(size);
+  return isValid(id) ? id : sweetid(size);
 }
