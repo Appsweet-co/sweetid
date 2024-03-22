38a1b8ce640fb5541f614795a301bc93884d895a
Derek Peruo <derek@appsweet.co>
2024-03-22 09:12:05 -0500

    chore: (deps.ts) Update Deno Standard Library import

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export type { 
   Args, 
   ParseOptions 
-} from "https://deno.land/std@0.220.1/flags/mod.ts";
+} from "https://deno.land/std@0.220.1/cli/parse_args.ts";
 export { customAlphabet } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
 export { parse } from "https://deno.land/std@0.220.1/flags/mod.ts";
