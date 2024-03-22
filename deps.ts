9c6b9812d1223bdf3af00d98e6157ebdca757963
Derek Peruo <derek@appsweet.co>
2024-03-22 09:15:00 -0500

    chore: (deps.ts) Update Deno imports

--- deps.ts
+++ deps.ts
@@ -1,6 +1,2 @@
-export type { 
-  Args, 
-  ParseOptions 
-} from "https://deno.land/std@0.220.1/cli/parse_args.ts";
+export { parse, type Args, type ParseOptions } from "https://deno.land/std@0.220.1/cli/parse_args.ts";
 export { customAlphabet } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
-export { parse } from "https://deno.land/std@0.220.1/flags/mod.ts";
